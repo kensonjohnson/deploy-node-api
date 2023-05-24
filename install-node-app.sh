72da1b953e44be8e2a2b801d221cce776858d122
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-23 19:33:34 -0500

    Finish install for webapp

--- install-node-app.sh
+++ install-node-app.sh
@@ -7,18 +7,20 @@ if (! which git >/dev/null); then
     sudo apt install git -y
 fi
 
-# Fast Node Manager (fnm)
-if (! which fnm >/dev/null); then
-    # Install dependencies
-    sudo apt install curl unzip
-
-    # Download and run script for fnm
-    curl -fsSL https://fnm.vercel.app/install | bash
-    source source ~/.bashrc
-fi
-
 # Node
 if (! which node >/dev/null); then
-    # --lts installs the current Long Term Support version
-    fnm install --lts
+    curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
+    sudo apt install -y nodejs
+fi
+
+# ========== Clone and Setup WebApp ========== #
+
+# Look for directory in expected path
+if [! -d ~/webapp]; then
+    # Directory not present: clone repo, install node packages, and build frontend (React)
+    git clone https://github.com/kensonjohnson/vite-express-project.git ~/webapp
+    pushd ~/webapp
+    npm install
+    npm run build
+    popd
 fi
