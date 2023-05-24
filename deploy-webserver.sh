fe0bde2facdc9a46d6ce48dd26e25a2a3f0cffc3
Kenson Johnson <kenson.johnson@outlook.com>
2023-05-23 22:29:15 -0500

    Created deploy script for local machine

---
+++ deploy-webserver.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# ========== Setup Dependencies ========== #
+
+# SSH keys
+if [! -f id_ed25519 ]; then
+    # Keys not present
+    ssh-keygen -t ed25519 -q -f id_ed25519 -C "developer@relative.path" -N ""
+fi
+
+# Cloud-Init
+if [! -f cloud-init.yaml ]; then
+    # cloud-init.yaml not present
+    cat <<-EOF >cloud-init.yaml
+users:
+  - default
+  - name: developer
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    ssh_authorized_keys:
+      - $(cat id_ed25519.pub)
+EOF
+fi
+
+# Snap
+if (! which snap >/dev/null); then
+    # Snap not present
+    sudo apt update && sudo apt install snapd
+fi
+
+# Multipass
+if (! multipass version >/dev/null); then
+    # Multipass not present
+    sudo snap install multipass
+fi
+
+# ========== Create or Start VM ========== #
+
+# Check if Ubuntu VM Exists
+if (! multipass info quotes >/dev/null); then
+    # VM not present
+    multipass launch --name quotes --cloud-init cloud-init.yaml --disk 20G --cpus 4 --memory 4G
+
+    # Use scp to transfer setup-server script to VM
+    scp -i id_ed25519 -o StrictHostKeyChecking=no setup-server.sh developer@$(multipass info quotes | grep IPv4 | awk '{print $2}'):/home/developer
+
+    # Run setup-server script
+    ssh -i id_ed25519 -o StrictHostKeyChecking=no developer@$(multipass info quotes | grep IPv4 | awk '{print $2}') 'bash setup-server.sh'
+fi
+
+# Check if VM in "Running" state
+if (multipass info quotes | grep Running >/dev/null); then
+    # VM not running
+    multipass start quotes
+fi
